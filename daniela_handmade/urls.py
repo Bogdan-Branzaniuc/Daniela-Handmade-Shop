c3697b5c58c6201914dabe430b6c4d92a0028687
Bogdan-Branzaniuc <>
2023-06-24 18:54:35 +0000

    re-aranged and customized the bootstrap navbar

--- daniela_handmade/urls.py
+++ daniela_handmade/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('profile/', include('profiles.urls')),
     path('unicorn/', include('django_unicorn.urls')),
 ]
