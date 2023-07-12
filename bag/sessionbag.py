6f67a2e48cccba7fa73420f304c24c5d082e75b9
Bogdan-Branzaniuc <>
2023-07-12 10:12:08 +0000

    created product component

--- bag/sessionbag.py
+++ bag/sessionbag.py
@@ -11,7 +11,7 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
     context = {
-
+        'bag': bag,
     }
 
     return context
--- products/components/product.py
+++ products/components/product.py
@@ -1,11 +1,12 @@
 from django_unicorn.components import UnicornView
 
 
-class BagstatusView(UnicornView):
+class ProductView(UnicornView):
+
     def mount(self, *args, **kwargs):
         return super().mount()
 
-    def add_to_bag(self, product_id):
+    def product_test(self, product_id):
         bag = self.request.session.get('bag', {})
         bag[product_id] = 1
         print(bag)
