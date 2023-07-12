ab46c9f9b8f8f31df1175572eb1ddc85bb4fefd3
Bogdan-Branzaniuc <>
2023-07-12 15:15:08 +0000

    experimented with child-parent relationship between django-unicorn components bag and product

--- bag/components/product_to_bag.py
+++ bag/components/product_to_bag.py
@@ -1,24 +1,24 @@
 from django_unicorn.components import UnicornView
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
-from django.shortcuts import get_object_or_404
 
 
-class BagstatusView(UnicornView):
+class ProductToBagView(UnicornView):
 
     bag = None
-    grand_total = 0
 
     def mount(self, *args, **kwargs):
         self.bag = self.request.session.get('bag', {})
-
         return super().mount()
 
-    def add_to_bag(self, product_id):
-        product = get_object_or_404(Product, id=product_id)
-        if product:
-            self.grand_total += int(product.price)
+    def add_item_to_bag(self, product_id):
+
+        product = get_object_or_404(Product, id=1)
 
         bag = self.request.session.get('bag', {})
         bag[product_id] = 1
-
+        self.call('rotate')
+        # self.parent.add_to_bag(self, product_id)
+        print(product)
         print(bag)
+        print(self.parent, '123')
--- products/components/product.py
+++ products/components/product.py
@@ -1,12 +1,13 @@
 from django_unicorn.components import UnicornView
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 
 class ProductView(UnicornView):
 
-    def mount(self, *args, **kwargs):
-        return super().mount()
+    product = None
 
-    def product_test(self, product_id):
-        bag = self.request.session.get('bag', {})
-        bag[product_id] = 1
-        print(bag)
+    def mount(self, *args, **kwargs):
+        self.product = get_object_or_404(Product, id=1)
+        print(self)
+        return super().mount()
