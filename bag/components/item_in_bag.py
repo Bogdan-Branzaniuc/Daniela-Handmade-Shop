799242bfa5d3264b8f9771e92c0b4de8d12ee46c
Bogdan Branzaniuc <bbranzaniuc53@gmail.com>
2023-07-23 23:02:47 +0100

    created two more methods in bagcallers.js enableEditingMode and DisableEditingMode, for displaying different states of the item_in_bag component

--- bag/components/item_in_bag.py
+++ bag/components/item_in_bag.py
@@ -6,16 +6,24 @@ class ItemInBagView(UnicornView):
     Unicorn Component view that handles the user interactions with the bag
     """
     editing = None
-
-
     def mount(self, *args, **kwargs):
-        self.editing = False
+        self.editing = 'false'
         self.user_bag = self.request.session.get('bag', {})
+        self.editig_bag_item_mode()
+
         return super().mount()
 
     def editing_product(self):
-        self.editing = True
-        print(self.editing)
+        self.editing = 'true'
+        self.editig_bag_item_mode()
+
 
     def submit_changes(self):
-        self.editing = False
+        self.editing = 'false'
+        self.editig_bag_item_mode()
+
+    def editig_bag_item_mode(self):
+        if self.editing == 'true':
+            self.call('enableEditingMode', self.item_index)
+        elif self.editing == 'false':
+            self.call('disableEditingMode', self.item_index)
--- bag/static/bag/js/bag_callers.js
+++ bag/static/bag/js/bag_callers.js
@@ -3,21 +3,18 @@ for( let button of document.querySelectorAll('.add-to-bag-btn')){
     button.addEventListener('click', (e) => {
         setTimeout(()=>{
             let productId = button.id.split('-')[4]
-            console.log(productId )
-
             let size = selectedSizeJs
             let color = selectedColorJs
             let qty = Number(document.querySelector(`#id_qty_${productId}`).value)
             Unicorn.call('bagstatus', 'add_to_bag', productId, size, color, qty)
         },300)
-
     })
 }
 for(let button of document.querySelectorAll('.remove-from-bag-btn')){
     button.addEventListener('click', (e) => {
         setTimeout(()=>{
             let productId = button.id.split('-')[4]
-            console.log(productId )
+            console.log(productId)
             let size = selectedSizeJs
             let color = selectedColorJs
             Unicorn.call('bagstatus', 'remove_from_bag', productId, size, color)
@@ -32,6 +29,7 @@ for (let button of document.querySelectorAll('.adjust-the-bag-btn')){
             let size = selectedSizeJs
             let color = selectedColorJs
             let qty = Number(document.querySelector(`#id_qty_${productId}`).value)
+            console.log('adjust')
             if(qty > 0){
                 Unicorn.call('bagstatus', 'add_to_bag', productId, size, color, qty)
             }else{
@@ -41,3 +39,11 @@ for (let button of document.querySelectorAll('.adjust-the-bag-btn')){
     })
 }
 
+function enableEditingMode(index){
+    $('#editing-bag-item-div-' + index).css('display', 'block')
+    $('#bag-item-data-div-' + index).css('display', 'none')
+}
+function disableEditingMode(index){
+    $('#editing-bag-item-div-' + index).css('display', 'none')
+    $('#bag-item-data-div-' + index).css('display', 'block')
+}
